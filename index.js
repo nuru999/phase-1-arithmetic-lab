b0bf6c0fe5eaf8ba6755f29d534e3301bba40c8e
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-09-25 12:38:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10)
+console.log(random)
+const num3 = 24;
+const num4 = 5;
+const mod =num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
+
+console.log ('multiply: ${multiply}');
+console.log ('random: ${random}');
+console.log ('mod: ${random}');
+console.log ('max: ${max}');
